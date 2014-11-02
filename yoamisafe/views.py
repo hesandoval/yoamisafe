c7f83995100807ff6a2abbb484ddf28cec24df1a
Hector Sandoval <hesandova@gmail.com>
2014-11-01 22:13:08 -0700

    url is parsed with location data as well as username

--- yoamisafe/views.py
+++ yoamisafe/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def map(request, username, longitude, latitude):
-    print "Username: ", username
-    print "longitude: ", longitude
-    print "latitude: ", latitude
+def map(request):
+    for res in request.GET:
+        if(request.GET[res] == ""):
+            #print "shit is empty yo"
+            print res
+            continue
+        print request.GET[res]
+
+    return HttpResponse(request.GET)
 
 def temp(request):
     return HttpResponse("Hello from temp")
